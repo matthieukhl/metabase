d00edffa81c530cc656d46fc63189d5b873ad165
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-05-14 20:03:32 -0400

    Reset file

--- frontend/src/metabase/browse/components/EllipsifiedWithMarkdownTooltip.tsx
+++ frontend/src/metabase/browse/components/EllipsifiedWithMarkdownTooltip.tsx
@@ -1,24 +1,20 @@
 import { Ellipsified } from "metabase/core/components/Ellipsified";
-import type { EllipsifiedProps } from "metabase/core/components/Ellipsified/Ellipsified";
 import Markdown from "metabase/core/components/Markdown";
-import { MultilineEllipsified } from "./BrowseModels.styled";
 
 export const EllipsifiedWithMarkdownTooltip = ({
   children,
-  multiline,
-  ...props
 }: {
   children: string;
-  multiline?: boolean;
-} & EllipsifiedProps) => {
-  props.tooltip = (
-    <Markdown disallowHeading unstyleLinks lineClamp={12}>
-      {children.replace(/\s/g, " ")}
-    </Markdown>
-  );
-  return multiline ? (
-    <MultilineEllipsified {...props}>{children}</MultilineEllipsified>
-  ) : (
-    <Ellipsified {...props}>{children}</Ellipsified>
+}) => {
+  return (
+    <Ellipsified
+      tooltip={
+        <Markdown disallowHeading unstyleLinks lineClamp={12}>
+          {children}
+        </Markdown>
+      }
+    >
+      {children}
+    </Ellipsified>
   );
 };
