061b41f897714219fbcdefc71f10ce17c3a835fa
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-05-15 09:05:32 -0400

    Move WeakSearchResult

--- frontend/src/metabase/browse/types.ts
+++ frontend/src/metabase/browse/types.ts
@@ -1,5 +1,16 @@
-import type { RecentCollectionItem } from "metabase-types/api";
-import type { WeakSearchResult } from "metabase-types/entities/search";
+import type {
+  RecentCollectionItem,
+  SearchModel,
+  SearchResult,
+  SearchResultId,
+} from "metabase-types/api";
+
+/** SearchResult with most properties optional */
+export type WeakSearchResult<
+  Id extends SearchResultId = SearchResultId,
+  Model extends SearchModel = SearchModel,
+> = Partial<SearchResult<Id, Model>> &
+  Pick<SearchResult<Id, Model>, "id" | "name" | "model">;
 
 /** Model retrieved through the search endpoint */
 export type ModelResult = WeakSearchResult<number, "dataset">;
