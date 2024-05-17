9acd9074795da32feed2695a50fafc4dfe8bfa86
John Swanson <john.swanson@metabase.com>
2024-05-17 11:06:45 -0700

    Rip out all the `trashed_from_collection_id`

--- src/metabase/search/scoring.clj
+++ src/metabase/search/scoring.clj
@@ -336,7 +336,6 @@
          :all-scores
          :relevant-scores
          :collection_effective_ancestors
-         :trashed_from_collection_id
          :collection_id
          :collection_location
          :collection_name
