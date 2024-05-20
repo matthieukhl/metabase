8aa961731a1c3afaf0bfa65facc1ff1f09bd0914
Emmad Usmani <emmadusmani@berkeley.edu>
2024-05-20 13:19:18 -0700

    fix labels not showing during emphasis

--- frontend/src/metabase/visualizations/echarts/cartesian/option/series.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/series.ts
@@ -333,7 +333,7 @@ const buildEChartsBarSeries = (
   if (seriesOption?.label != null) {
     seriesOption.label.show = false;
   }
-  if (seriesOption?.emphasis?.label != null) {
+  if (seriesOption?.emphasis != null) {
     seriesOption.emphasis.label = { show: true };
   }
 
